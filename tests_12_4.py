d3e523d659d92da000c3fbcc36d87d3e4b519da0
MarinaByhalova <mzhirnovsk@gmail.com>
2024-12-01 14:57:36 +0300

    Add files via upload

---
+++ tests_12_4.py
@@ -0,0 +1,92 @@
+import logging
+import unittest
+
+
+class Runner:
+    def __init__(self, name, speed=5):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
+        self.distance = 0
+        if speed > 0:
+            self.speed = speed
+        else:
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+
+# first = Runner('Вося', 10)
+# second = Runner('Илья', 5)
+# # third = Runner('Арсен', 10)
+#
+# t = Tournament(101, first, second)
+# print(t.start())
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @classmethod
+    def setUpClass(cls):
+        logging.basicConfig(filename='runner_tests.log', filemode='w', level=logging.INFO, encoding='utf-8',
+                            format='%(asctime)s | %(levelname)s | %(message)s')
+
+    def test_walk(self):
+        try:
+            runner1 = Runner(name='ran1', speed=-15)
+            for i in range(10):
+                runner1.walk()
+            self.assertEqual(runner1.distance, 50)
+        except ValueError:
+            logging.warning('Неверная скорость для Runner')
+        logging.info('test_walk" выполнен успешно')
+
+    def test_run(self):
+        try:
+            runner2 = Runner(10, 11)
+            for i in range(10):
+                runner2.run()
+            self.assertEqual(runner2.distance, 100)
+        except TypeError:
+            logging.warning('Неверный тип данных для объекта Runner')
+        logging.info('"test_run" выполнен успешно')
+
+
+if __name__ == '__main__':
+    unittest.main()
